e8be65dc9f49eb4398534cf17ca89a576db41e13
user <user@ST-27>
2012-11-02 10:20:13 +0900

    버튼

---
+++ src/com/example/test2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.test2;
+
+import org.w3c.dom.Text;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	Button btn1;
+	TextView txn1;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btn1 = (Button)findViewById(R.id.button1);
+        txn1 = (TextView)findViewById(R.id.textview1);
+        btn1.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		txn1.setText("나는안드로이드");
+		
+	}
+}
